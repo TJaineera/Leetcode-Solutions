b04b19efad19ce5dd8092592a0621a8b8ae8b20a
Twinkle Jaineera <165087584+TJaineera@users.noreply.github.com>
2025-10-24 11:53:57 -0500

    Add remove-duplicates-from-sorted-list (remove-duplicates-from-sorted-list) solution in java

---
+++ leetcode/remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -0,0 +1,23 @@
+/**
+ * Title: remove-duplicates-from-sorted-list
+ * Slug: remove-duplicates-from-sorted-list
+ * URL: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ * Language: java
+ * Submitted: 2025-10-24T16:53:56.801Z
+ */
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
