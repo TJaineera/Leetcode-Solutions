92410f3a641b0d398a6888f32afe0a319064c9ff
Twinkle Jaineera <165087584+TJaineera@users.noreply.github.com>
2025-10-24 10:46:38 -0500

    Add sort-an-array (sort-an-array) solution in java

---
+++ leetcode/sort-an-array/sort-an-array.java
@@ -0,0 +1,14 @@
+/**
+ * Title: sort-an-array
+ * Slug: sort-an-array
+ * URL: https://leetcode.com/problems/sort-an-array/?envType=problem-list-v2&envId=bucket-sort
+ * Language: java
+ * Submitted: 2025-10-24T15:46:37.598Z
+ */
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
