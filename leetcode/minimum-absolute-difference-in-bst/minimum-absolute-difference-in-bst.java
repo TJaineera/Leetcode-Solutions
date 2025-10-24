207a943d9a11b110039f4cc1f8300618af0ad786
Twinkle Jaineera <165087584+TJaineera@users.noreply.github.com>
2025-10-24 11:13:28 -0500

    Add minimum-absolute-difference-in-bst (minimum-absolute-difference-in-bst) solution in java

---
+++ leetcode/minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.java
@@ -0,0 +1,46 @@
+/**
+ * Title: minimum-absolute-difference-in-bst
+ * Slug: minimum-absolute-difference-in-bst
+ * URL: https://leetcode.com/problems/minimum-absolute-difference-in-bst/?envType=problem-list-v2&envId=binary-search-tree
+ * Language: java
+ * Submitted: 2025-10-24T16:13:27.718Z
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    private Integer prev = null;
+    private int minDiff = Integer.MAX_VALUE;
+
+    public int getMinimumDifference(TreeNode root) {
+        inorder(root);
+        return minDiff;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+
+        inorder(node.left);
+
+        if (prev != null) {
+            minDiff = Math.min(minDiff, node.val - prev);
+        }
+        prev = node.val;
+
+        inorder(node.right);
+    }
+}
