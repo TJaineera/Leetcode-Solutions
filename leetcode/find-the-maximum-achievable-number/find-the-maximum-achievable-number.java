db12a5a955619d78fa426bbe73124a67e74d2507
Twinkle Jaineera <165087584+TJaineera@users.noreply.github.com>
2025-10-28 16:06:09 -0500

    Add find-the-maximum-achievable-number (find-the-maximum-achievable-number) solution in java

---
+++ leetcode/find-the-maximum-achievable-number/find-the-maximum-achievable-number.java
@@ -0,0 +1,13 @@
+/**
+ * Title: find-the-maximum-achievable-number
+ * Slug: find-the-maximum-achievable-number
+ * URL: https://leetcode.com/problems/find-the-maximum-achievable-number/?envType=problem-list-v2&envId=math
+ * Language: java
+ * Submitted: 2025-10-28T21:06:08.745Z
+ */
+
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+}
