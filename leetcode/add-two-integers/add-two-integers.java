fd07a270c1858f7e67f535958e2a12083611cf72
Twinkle Jaineera <165087584+TJaineera@users.noreply.github.com>
2025-10-28 16:09:20 -0500

    Add add-two-integers (add-two-integers) solution in java

---
+++ leetcode/add-two-integers/add-two-integers.java
@@ -0,0 +1,13 @@
+/**
+ * Title: add-two-integers
+ * Slug: add-two-integers
+ * URL: https://leetcode.com/problems/add-two-integers/?envType=problem-list-v2&envId=math
+ * Language: java
+ * Submitted: 2025-10-28T21:09:19.358Z
+ */
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
